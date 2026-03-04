3f33f18eeb8a218e5c657f5a53a250127c868f27
helloaxis <hello.coreaxis@gmail.com>
2026-03-04 23:39:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ app = Flask(name)
 
 @app.route("/")
 def home():
-return """
-<h2>Valentine Glyph</h2>
-<form action="/test">
-<input name="date1" placeholder="YYYY-MM-DD" required>
-<input name="date2" placeholder="YYYY-MM-DD" required>
-<button type="submit">Test</button>
-</form>
+    return """
+    <h2>Valentine Glyph</h2>
+    <form action="/test">
+        <input name="date1" placeholder="YYYY-MM-DD" required>
+        <input name="date2" placeholder="YYYY-MM-DD" required>
+        <button type="submit">Test</button>
+    </form>
+     """
 @app.route("/test")
 def test():
-return "FORM WORKS"
+    return "FORM WORKS"
 
-if name == "main":
+if __name__== "__main__":
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
