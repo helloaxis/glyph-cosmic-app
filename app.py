a0a52f774f0267d26066c1e00ac3fd69f2fe856e
helloaxis <hello.coreaxis@gmail.com>
2026-03-04 22:41:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,1 @@
-import os
-from flask import Flask
-
-app = Flask(__name__)
-
-def digit_sum(n):
-        x = 0
-    for c in str(n):
-        if c.isdigit():
-            x = x + int(c)
-    return x
-
-@app.route("/")
-def home():
-    return "WORKING"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+raise Exception("TEST")
