700baac2f30576e1d2e6b22dac4ecf4b1a427aa4
helloaxis <hello.coreaxis@gmail.com>
2026-03-04 22:46:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-raise Exception("HELLO_TEST")
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "WORKING"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
